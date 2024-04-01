7dbaad35a1af19420c814cd915cdde1223261821
Mohd-Mahin <mahinit3016@gmail.com>
2024-04-01 17:26:53 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const publicDirPath = path.join(__dirname, "..", "public");
+
+app.use(express.static(publicDirPath));
+
+app.listen(PORT, () => console.log(`Listening on Port ${PORT} !!!`));
